d1df65d7097001a39a389e1849382c6790fe485b
spmoroney <spmoroney@att.net>
2023-07-14 21:35:24 -0400

    Created object to store character sets and logic to generate a password.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var characterSets = {
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  numbers: "1234567890",
+  specialChars: " !\"#$%&\'()*+-./:;<=>@[\\]^_{|}~",
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +19,43 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var passwordLength = prompt("How long do you want your password to be?");
+
+  console.log(typeof(passwordLength));
+  if (isNaN(passwordLength) || Number(passwordLength) < 8 || Number(passwordLength) > 128) {
+    alert("Invalid Entry, please enter a number between 8 and 128 for the length.");
+    return "Invalid Entry, Please Try Again";
+  }
+
+  var useUpperCase = confirm("Do you want to use upper case letters in your password?");
+  var useLowerCase = confirm("Do you want to use lower case letters in your password?");
+  var useNumbers = confirm("Do you want to use numbers in your password?");
+  var useSpecialCharacters = confirm("Do you want to use special characters in your password?");
+  var generatedPassword = "";
+
+
+  var selectedCharacterSet= ""
+
+  if (useUpperCase) {
+    selectedCharacterSet += characterSets.upperCase;
+  }
+  if (useLowerCase) {
+    selectedCharacterSet += characterSets.lowerCase;
+  }
+  if (useNumbers) {
+    selectedCharacterSet += characterSets.numbers;
+  }
+  if (useSpecialCharacters) {
+    selectedCharacterSet += characterSets.specialChars;
+  }
+
+  for (var i = 0; i <= passwordLength; i++) {
+    var selectedIndex = Math.floor(Math.random() * selectedCharacterSet.length);
+    generatedPassword += selectedCharacterSet[selectedIndex];
+  }
+
+  return generatedPassword;
+
+}
